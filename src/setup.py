2e077ffb3eb338d55d21308222bcdc2ed34ed7fa
JamesStrabala <125584048+JamesStrabala@users.noreply.github.com>
2025-05-02 19:47:11 -0500

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,6 @@ def load_fasta_sequences(path):
     for filename in os.listdir(path):
         if filename.endswith(".fasta"):
             file_path = os.path.join(path, filename)
-            print('reading' +str(file_path))
             with open(file_path, "r") as f:
                 seq = ""
                 for line in f:
@@ -17,11 +16,11 @@ def load_fasta_sequences(path):
                         seq += line.strip()
                 if seq and len (sec)<=max_len:
                     sequences.append(seq)
+    print(str(len(sequences))+" sequences loaded.")
     return sequences
 
 def build_vocab():
     amino_acids = "ACDEFGHIKLMNPQRSTVWY"
     vocab = {aa: idx+1 for idx, aa in enumerate(amino_acids)}  # 0 reserved for padding
-    print("Made vocabulary")
     return vocab
 #Sequences\Voltage 588K.fasta
